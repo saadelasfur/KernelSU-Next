6100df6f8c019eaf09fb84d681e057ec22bff4d9
rifsxd <rifat.44.azad.rifs@gmail.com>
2025-02-24 20:32:46 +0600

    manager: remove redundant enabled state check in SettingScreen

--- manager/app/src/main/java/com/rifsxd/ksunext/ui/screen/Settings.kt
+++ manager/app/src/main/java/com/rifsxd/ksunext/ui/screen/Settings.kt
@@ -178,8 +178,7 @@ fun SettingScreen(navigator: DestinationsNavigator) {
                     icon = Icons.Filled.RemoveModerator,
                     title = stringResource(id = R.string.settings_disable_su),
                     summary = stringResource(id = R.string.settings_disable_su_summary),
-                    checked = isSuDisabled,
-                    enabled = !isSuDisabled // we can't re-enable su if it's disabled.
+                    checked = isSuDisabled
                 ) { checked ->
                     val shouldEnable = !checked
                     if (Natives.setSuEnabled(shouldEnable)) {
