a8bfd1cc7dae37a33f34a7c34b683516bff9df7c
Rifat Azad <rifat.44.azad.rifs@gmail.com>
2025-07-02 15:43:21 +0600

    manager:added more dynamic color vars to webui-next

--- manager/app/src/main/java/com/rifsxd/ksunext/ui/webui/MonetColorsProvider.kt
+++ manager/app/src/main/java/com/rifsxd/ksunext/ui/webui/MonetColorsProvider.kt
@@ -6,7 +6,9 @@ import android.os.Build
 
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.unit.dp
 
 /**
  * @author rifsxd
@@ -25,15 +27,52 @@ object MonetColorsProvider {
         }
 
         val monetColors = mapOf(
+            // App Base Colors
             "primary" to colorScheme.primary.toArgb().toHex(),
             "onPrimary" to colorScheme.onPrimary.toArgb().toHex(),
+            "primaryContainer" to colorScheme.primaryContainer.toArgb().toHex(),
+            "onPrimaryContainer" to colorScheme.onPrimaryContainer.toArgb().toHex(),
+            "inversePrimary" to colorScheme.inversePrimary.toArgb().toHex(),
+            "secondary" to colorScheme.secondary.toArgb().toHex(),
+            "onSecondary" to colorScheme.onSecondary.toArgb().toHex(),
+            "secondaryContainer" to colorScheme.secondaryContainer.toArgb().toHex(),
+            "onSecondaryContainer" to colorScheme.onSecondaryContainer.toArgb().toHex(),
+            "tertiary" to colorScheme.tertiary.toArgb().toHex(),
+            "onTertiary" to colorScheme.onTertiary.toArgb().toHex(),
+            "tertiaryContainer" to colorScheme.tertiaryContainer.toArgb().toHex(),
+            "onTertiaryContainer" to colorScheme.onTertiaryContainer.toArgb().toHex(),
             "background" to colorScheme.background.toArgb().toHex(),
+            "onBackground" to colorScheme.onBackground.toArgb().toHex(),
+            "surface" to colorScheme.surface.toArgb().toHex(),
+            "tonalSurface" to colorScheme.surfaceColorAtElevation(1.dp).toArgb().toHex(), // surfaceColorAtElevation(1.dp) not available here
             "onSurface" to colorScheme.onSurface.toArgb().toHex(),
+            "surfaceVariant" to colorScheme.surfaceVariant.toArgb().toHex(),
             "onSurfaceVariant" to colorScheme.onSurfaceVariant.toArgb().toHex(),
-            "tonalSurface" to colorScheme.surface.toArgb().toHex(),
-            "surfaceBright" to colorScheme.surfaceVariant.toArgb().toHex(),
+            "surfaceTint" to colorScheme.surfaceTint.toArgb().toHex(),
+            "inverseSurface" to colorScheme.inverseSurface.toArgb().toHex(),
+            "inverseOnSurface" to colorScheme.inverseOnSurface.toArgb().toHex(),
+            "error" to colorScheme.error.toArgb().toHex(),
+            "onError" to colorScheme.onError.toArgb().toHex(),
+            "errorContainer" to colorScheme.errorContainer.toArgb().toHex(),
+            "onErrorContainer" to colorScheme.onErrorContainer.toArgb().toHex(),
+            "outline" to colorScheme.outline.toArgb().toHex(),
             "outlineVariant" to colorScheme.outlineVariant.toArgb().toHex(),
-            "error" to colorScheme.error.toArgb().toHex()
+            "scrim" to colorScheme.scrim.toArgb().toHex(),
+            "surfaceBright" to colorScheme.surfaceBright.toArgb().toHex(),
+            "surfaceDim" to colorScheme.surfaceDim.toArgb().toHex(),
+            "surfaceContainer" to colorScheme.surfaceContainer.toArgb().toHex(),
+            "surfaceContainerHigh" to colorScheme.surfaceContainerHigh.toArgb().toHex(),
+            "surfaceContainerHighest" to colorScheme.surfaceContainerHighest.toArgb().toHex(),
+            "surfaceContainerLow" to colorScheme.surfaceContainerLow.toArgb().toHex(),
+            "surfaceContainerLowest" to colorScheme.surfaceContainerLowest.toArgb().toHex(),
+            "filledTonalButtonContentColor" to colorScheme.onPrimaryContainer.toArgb().toHex(),
+            "filledTonalButtonContainerColor" to colorScheme.secondaryContainer.toArgb().toHex(),
+            "filledTonalButtonDisabledContentColor" to colorScheme.onSurfaceVariant.toArgb().toHex(),
+            "filledTonalButtonDisabledContainerColor" to colorScheme.surfaceVariant.toArgb().toHex(),
+            "filledCardContentColor" to colorScheme.onPrimaryContainer.toArgb().toHex(),
+            "filledCardContainerColor" to colorScheme.primaryContainer.toArgb().toHex(),
+            "filledCardDisabledContentColor" to colorScheme.onSurfaceVariant.toArgb().toHex(),
+            "filledCardDisabledContainerColor" to colorScheme.surfaceVariant.toArgb().toHex()
         )
 
         return monetColors.toCssVars()
